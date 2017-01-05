8e776344f3e754fb4d5405685de2b3643b381d07
Jevgenij Tsoi <jevgenij@ordolabs.io>
2017-01-05 22:11:54 +0100

    Added svtplaydl

---
+++ roles/server-nuc/files/svtplaydl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p /nas/Video/Kids/bondgarden
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/bondgarden/ --silent-semi http://www.svtplay.se/bondgarden
+
+mkdir -p /nas/Video/Kids/stora-maskiner
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/stora-maskiner/ --silent-semi http://www.svtplay.se/stora-maskiner
+
+mkdir -p /nas/Video/Kids/bamse
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/bamse/ --silent-semi http://www.svtplay.se/bamse---varldens-starkaste-bjorn
+
+mkdir -p /nas/Video/Kids/bajsfilmen
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/bajsfilmen/ --silent-semi http://www.svtplay.se/bajsfilmen-dolores-och-gunellens-varld
+
+mkdir -p /nas/Video/Kids/skrap
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/skrap/ --silent-semi http://www.svtplay.se/skrap
+
+mkdir -p /nas/Video/Kids/jycke
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/jycke/ --silent-semi http://www.svtplay.se/hej-jycke
+
+mkdir -p /nas/Video/Kids/bing
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/bing/ --silent-semi http://www.svtplay.se/bing
