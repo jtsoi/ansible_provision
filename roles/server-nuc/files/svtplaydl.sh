095d5bf6e92a09f4ab224e45f92173123ab0ab47
Jevgenij Tsoi <jevgenij@ordolabs.io>
2017-03-21 09:23:42 +0100

    Added more shows.

--- roles/server-nuc/files/svtplaydl.sh
+++ roles/server-nuc/files/svtplaydl.sh
@@ -20,3 +20,24 @@ mkdir -p /nas/Video/Kids/jycke
 
 mkdir -p /nas/Video/Kids/bing
 /usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/bing/ --silent-semi http://www.svtplay.se/bing
+
+mkdir -p /nas/Video/Kids/vem
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/vem/ --silent-semi http://www.svtplay.se/vem
+
+mkdir -p /nas/Video/Kids/ronja-rovardotter
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/ronja-rovardotter/ --silent-semi http://www.svtplay.se/ronja-rovardotter
+
+mkdir -p /nas/Video/Kids/johans-fordon
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/johans-fordon/ --silent-semi http://www.svtplay.se/johans-fordon
+
+mkdir -p /nas/Video/Kids/alfons-aberg
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/alfons-aberg/ --silent-semi http://www.svtplay.se/alfons-aberg
+
+mkdir -p /nas/Video/Kids/pippi-langstrump
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/pippi-langstrump/ --silent-semi http://www.svtplay.se/pippi-langstrump
+
+mkdir -p /nas/Video/Kids/igelkotten-som-delade-med-sig
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/igelkotten-som-delade-med-sig/ --silent-semi http://www.svtplay.se/igelkotten-som-delade-med-sig
+
+mkdir -p /nas/Video/Kids/henry-rakar-pa
+/usr/local/bin/svtplay-dl -A -t -o /nas/Video/Kids/henry-rakar-pa/ --silent-semi http://www.svtplay.se/henry-rakar-pa
